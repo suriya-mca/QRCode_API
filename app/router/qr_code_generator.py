0051ecbc9f7d98f31b3cf05f1775952d9202a30d
Suriya <suriyag1999@gmail.com>
2024-04-28 20:00:56 +0530

    converted html response to streaming response

--- app/router/qr_code_generator.py
+++ app/router/qr_code_generator.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+import io
+from fastapi.responses import StreamingResponse
 import segno
 from segno import helpers
 
@@ -9,31 +9,36 @@ from schema.schemas import UrlData, WifiData, ContactData, GeoLocationData, Emai
 
 router = APIRouter()
 
-templates = Jinja2Templates(directory="templates")
-
 # URL to QR code generator function
 @router.post("/url_to_qr")
-async def url_to_qr_generator(request: Request, info: UrlData) -> HTMLResponse:
+async def url_to_qr_generator(request: Request, info: UrlData) -> StreamingResponse:
 
     data: str = info.url
-    qr_data: bytes = segno.make_qr(data, error = 'H') 
-    return templates.TemplateResponse("home.html", {"request": request, "qr_data": qr_data})
+    qr_data: bytes = segno.make_qr(data, error = 'H')
+    qr_buffer = io.BytesIO()
+    qr_data.save(qr_buffer, kind='png', scale=10)
+    qr_buffer.seek(0)
+    return StreamingResponse(content=qr_buffer, media_type="image/png")
+    # return templates.TemplateResponse("home.html", {"request": request, "qr_data": qr_data})
 
 
 # Wifi Data to QR code generator function
 @router.post("/wifi_to_qr")
-async def wifi_to_qr_generator(request: Request, info: WifiData) -> HTMLResponse:
+async def wifi_to_qr_generator(request: Request, info: WifiData) -> StreamingResponse:
 
     name: str = info.wifi_name
     password: str = info.wifi_password
     data: bytes = helpers.make_wifi_data(ssid=name, password=password, security='WPA')
     qr_data: bytes = segno.make(data, error = 'H')
-    return templates.TemplateResponse("home.html", {"request": request, "qr_data": qr_data})
+    qr_buffer = io.BytesIO()
+    qr_data.save(qr_buffer, kind='png', scale=10)
+    qr_buffer.seek(0)
+    return StreamingResponse(content=qr_buffer, media_type="image/png")
 
 
 # Contact details to QR code generator function
 @router.post("/contact_to_qr")
-async def contact_to_qr_generator(request: Request, info: ContactData) -> HTMLResponse:
+async def contact_to_qr_generator(request: Request, info: ContactData) -> StreamingResponse:
     
     data: bytes = helpers.make_vcard_data(
         displayname = info.name,
@@ -46,21 +51,27 @@ async def contact_to_qr_generator(request: Request, info: ContactData) -> HTMLRe
         url = info.url
     )
     qr_data: bytes = segno.make(data, error = 'H')
-    return templates.TemplateResponse("home.html", {"request": request, "qr_data": qr_data})
+    qr_buffer = io.BytesIO()
+    qr_data.save(qr_buffer, kind='png', scale=10)
+    qr_buffer.seek(0)
+    return StreamingResponse(content=qr_buffer, media_type="image/png")
 
 
 # Geographic location to QR code generator function
 @router.post("/geo_to_qr")
-async def geo_to_qr_generator(request: Request, info: GeoLocationData) -> HTMLResponse:
+async def geo_to_qr_generator(request: Request, info: GeoLocationData) -> StreamingResponse:
     
     data: bytes = helpers.make_geo_data(info.latitude, info.longitude)
     qr_data: bytes = segno.make(data, error = 'H')
-    return templates.TemplateResponse("home.html", {"request": request, "qr_data": qr_data})
+    qr_buffer = io.BytesIO()
+    qr_data.save(qr_buffer, kind='png', scale=10)
+    qr_buffer.seek(0)
+    return StreamingResponse(content=qr_buffer, media_type="image/png")
 
 
 # Email to QR QR code generator function
 @router.post("/email_to_qr")
-async def email_to_qr_generator(request: Request, info: EmailData) -> HTMLResponse:
+async def email_to_qr_generator(request: Request, info: EmailData) -> StreamingResponse:
     
     qr_data: bytes = helpers.make_email(
         to = info.to,
@@ -68,4 +79,7 @@ async def email_to_qr_generator(request: Request, info: EmailData) -> HTMLRespon
         body = info.body,
         cc = info.cc
     )
-    return templates.TemplateResponse("home.html", {"request": request, "qr_data": qr_data})
+    qr_buffer = io.BytesIO()
+    qr_data.save(qr_buffer, kind='png', scale=10)
+    qr_buffer.seek(0)
+    return StreamingResponse(content=qr_buffer, media_type="image/png")
