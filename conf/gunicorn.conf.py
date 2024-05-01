0215e0334659095459fe57fb60372b0c8f9b6d28
Suriya <suriyag1999@gmail.com>
2024-05-01 17:29:37 +0530

    minor change in gunicorn config

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 import uvicorn
 
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() + 1
 bind = '0.0.0.0:8000'
 worker_class = 'uvicorn.workers.UvicornWorker'
